424f88772751806cbf9e63dbc2c39c12b490fb3a
yamerlin <merlin.yann63@gmail.com>
2023-02-08 15:53:06 -0500

    Hello world

---
+++ src/bataille.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class bataille{
+
+    public static int [][] grilleOrdi = new int[10][10];
+    public static int [][] grilleJeu = new int[10][10];
+
+    public static boolean posOk(int [][]grille, int l, int c, int d, int t){
+        boolean isPosOk = false;
+
+        return isPosOk;
+    }
+
+    public static void initGrilleOrdi(){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
